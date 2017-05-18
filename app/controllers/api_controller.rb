7e659528717173b3b5a252633da1cbaad8cc79bf
Sarah Betack <msbetack@gmail.com>
2017-05-18 18:00:02 -0500

    can get place id from restaurants based on google query

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,10 @@
 class ApiController < ApplicationController
   def index
-    # @client = GooglePlaces::Client.new($GOOGLE_PLACES_API_KEY)
-    google_api = ENV["GOOGLE_PLACES_API_KEY"]
-    client = GooglePlaces::Client.new(google_api)
-    #@client.spots(latitude, longitude) is another option
+    client = GooglePlaces::Client.new(ENV["GOOGLE_PLACES_API_KEY"])
+    #you can also query by latitude and longitude client.spots(latitude, longitude)
     @restaurants = client.spots_by_query("Restaurants near Austin Texas")
+    # we will probably want to iterate through that ^^ to create out place items
+    
   end
 
 end
