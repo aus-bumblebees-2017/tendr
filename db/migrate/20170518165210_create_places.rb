019660d9c3893bbeda1553acc31200488c7137aa
Raymond Callahan <email": null>
2017-05-19 16:09:20 -0500

    Minor change to migration

--- db/migrate/20170518165210_create_places.rb
+++ db/migrate/20170518165210_create_places.rb
@@ -2,7 +2,8 @@ class CreatePlaces < ActiveRecord::Migration[5.0]
   def change
     create_table :places do |t|
       t.text :google_id
-
+      t.string :name
+      t.float :rating
       t.timestamps
     end
   end
