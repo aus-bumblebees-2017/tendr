6143e1e170b08f20a01867cf9a2545d1fa85a5b1
Benjamin Martin <benjamin.martin2010@gmail.com>
2017-05-21 13:41:11 +0000

    Add Place lookup to place model, add place/show/:id route, add place/show view and controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,3 +1,9 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
+
+  def show
+    @place = Place.find(params[:id])
+    @place_metadata = @place.get_place_info
+    current_user.last_viewed = @place.id
+  end
 end
