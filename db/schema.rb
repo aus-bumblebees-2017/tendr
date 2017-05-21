5be84d1d7f04d7247ed0cef79dac9c977aeeac54
Raymond <Callahan>
2017-05-21 14:58:59 -0500

    creating categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518165210) do
+ActiveRecord::Schema.define(version: 20170518221543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.integer  "place_id"
-    t.text     "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "google_place_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.index ["place_id"], name: "index_foods_on_place_id", using: :btree
   end
 
@@ -35,8 +35,6 @@ ActiveRecord::Schema.define(version: 20170518165210) do
 
   create_table "places", force: :cascade do |t|
     t.text     "google_id"
-    t.string   "name"
-    t.float    "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -54,7 +52,6 @@ ActiveRecord::Schema.define(version: 20170518165210) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "diet"
     t.integer  "zipcode"
     t.string   "user_name"
     t.text     "last_viewed"
