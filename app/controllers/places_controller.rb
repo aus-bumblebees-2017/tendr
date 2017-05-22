ef5582c87388654b8c4eb54dfdd52231fad180a0
Ben Martin <benjamin.martin2010@gmail.com>
2017-05-22 16:36:08 -0500

    Corrected.
    Fixed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,6 +5,6 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     @last_food = Food.find(params[:food_id])
     @place_metadata = @place.get_place_metadata
-    current_user.update_attribute(:last_viewed, @place.id)
+    current_user.update_attribute(:last_viewed, @last_food.id)
   end
 end
