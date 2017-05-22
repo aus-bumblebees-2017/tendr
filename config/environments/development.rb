003592a382b6e3243c731f200c8151838f5d7e72
Ben Martin <email": null>
2017-05-22 14:22:01 -0500

    Change config mail quotes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,13 +36,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-  address: “smtp.gmail.com”,
+  address: "smtp.gmail.com",
   port: 587,
-  domain: ENV[“GMAIL_DOMAIN”],
-  authentication: “plain”,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: 'plain',
   enable_starttls_auto: true,
-  user_name: ENV[“GMAIL_USERNAME”],
-  password: ENV[“GMAIL_PASSWORD”]
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
   }
 
   config.action_mailer.default_url_options = { :host => 'tendr-staging.herokuapp.com' }
